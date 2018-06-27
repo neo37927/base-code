ba43cd41ad38496c9094af5e97dd95d7769ac19e
xiaolin <@>
2018-06-27 15:45:14 +0800

    initial

---
+++ function-test/src/main/java/com/code/demo/FunctionTestApplication.java
@@ -0,0 +1,12 @@
+package com.code.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionTestApplication.class, args);
+	}
+}
