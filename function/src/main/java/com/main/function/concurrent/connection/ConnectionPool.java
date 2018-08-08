9683ebdf80e6cbc92b7b35286900fc9098e6a1e9
phoenix <null@123.com>
2018-08-08 16:49:26 +0800

    基础模块修改

---
+++ function/src/main/java/com/main/function/concurrent/connection/ConnectionPool.java
@@ -0,0 +1,19 @@
+package com.code.demo.concurrent.connection;
+
+import com.code.demo.concurrent.async.SingleResultCallback;
+
+import java.io.Closeable;
+import java.util.concurrent.TimeUnit;
+
+interface ConnectionPool extends Closeable {
+
+    InternalConnection get();
+
+    InternalConnection get(long timeout, TimeUnit timeUnit);
+
+    void getAsync(SingleResultCallback<InternalConnection> callback);
+
+    void invalidate();
+
+    void close();
+}
---
+++ function/src/main/java/com/main/function/concurrent/connection/ResponseBuffers.java
@@ -0,0 +1,52 @@
+package com.code.demo.concurrent.connection;
+
+import org.bson.ByteBuf;
+
+import java.io.Closeable;
+
+class ResponseBuffers implements Closeable {
+    private final ReplyHeader replyHeader;
+    private final ByteBuf bodyByteBuffer;
+    private volatile boolean isClosed;
+
+    /**
+     * Construct an instance.
+     *
+     * @param replyHeader the reply header
+     * @param bodyByteBuffer a byte buffer containing the message body
+     */
+    public ResponseBuffers(final ReplyHeader replyHeader, final ByteBuf bodyByteBuffer) {
+        this.replyHeader = replyHeader;
+        this.bodyByteBuffer = bodyByteBuffer;
+    }
+
+    /**
+     * Gets the reply header.
+     *
+     * @return the reply header
+     */
+    public ReplyHeader getReplyHeader() {
+        return replyHeader;
+    }
+
+    /**
+     * Returns a read-only buffer containing the response body.  Care should be taken to not use the returned buffer after this instance has
+     * been closed.
+     *
+     * @return a read-only buffer containing the response body
+     */
+    public ByteBuf getBodyByteBuffer() {
+        return bodyByteBuffer.asReadOnly();
+    }
+
+    @Override
+    public void close() {
+        if (!isClosed) {
+            if (bodyByteBuffer != null) {
+                bodyByteBuffer.release();
+            }
+            isClosed = true;
+        }
+    }
+}
+
