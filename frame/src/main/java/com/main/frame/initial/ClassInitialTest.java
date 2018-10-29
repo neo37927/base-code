20695d583ec9d5db32a11cd06d6e48d0d9c7ed7e
大好人 <null@123.com>
2018-10-29 10:32:52 +0800

    Class initial

---
+++ frame/src/main/java/com/main/frame/initial/ClassInitialTest.java
@@ -0,0 +1,26 @@
+package com.main.frame.initial;
+
+public class ClassInitialTest {
+    //静态区域的构建顺序，将影响一些变量的初始化过程
+    public static int counter2 = 0;
+
+    private static ClassInitialTest singleton = new ClassInitialTest();
+
+    public static int counter1;
+
+
+
+    private ClassInitialTest() {
+        counter1++;
+        counter2++;
+    }
+
+    public static ClassInitialTest getSingleton() {
+        return singleton;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getSingleton().counter1);
+        System.out.println(getSingleton().counter2);
+    }
+}
